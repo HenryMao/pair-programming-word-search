9f7c3a5c742d161f926fb09c40421e3120a6a4f9
HenryMao <henry60308@live.cn>
2020-07-02 19:36:52 +0000

    without stretch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    let temp = '';
+    let horizontalJoin = letters.map(ls => ls.join(''));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)){
+            return true
+        } 
     }
+    for(let i = 0 ; i < letters[0].length; i++){
+        for(let j = 0; j < letters.length; j++){
+            temp += letters[i][j];
+        }
+        if(temp === word){
+            return true;
+        }
+    }
+    return false;
 }
+    
+
+
 
 module.exports = wordSearch
